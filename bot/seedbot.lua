4814ce3f5b5eb227ce2d7a6fc90077440fd106d4
teleslayer <superslayerbot@gmail.com>
2016-02-19 13:11:37 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,27 +228,7 @@ function create_config( )
     },
     sudo_users = {147191022},--Sudo users
     disabled_channels = {},
-    moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    moderation = {data = 'data/moderation.json'},]
 ]],
     help_text_realm = [[
 Realm Commands:
